b47535c84cd50283a76698bea500eaa851e357a6
NINE-J <rnwo47@gmail.com>
2022-02-16 18:04:44 +0900

    install tailwindcss

---
+++ src/components/MainInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-input">
+    <input type="text" v-model="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainInput',
+  data() {
+    return {
+      text: undefined
+    };
+  }
+};
+</script>
+
+<style lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <h1>Todos</h1>
+    <MainInput></MainInput>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import MainInput from '@/components/MainInput.vue';
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
-  },
+    MainInput
+  }
 };
 </script>
